32815d04a611f9be8287cc0a3eb5a4816c419d90
Lucas <lucasfernandomacedo13@gmail.com>
2022-05-20 16:28:27 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { species: allAnimalsObjects } = data;
+
+function selecionaAnimal(animal) {
+  let selectedObject;
+  allAnimalsObjects.forEach((specieObject) => {
+    const { name: specieName } = specieObject;
+    if (animal === specieName) {
+      selectedObject = specieObject;
+    }
+  });
+  return selectedObject;
+}
+
+function confereIdade(age, selectedObject) {
+  const { residents } = selectedObject;
+  const arrayOfAges = [];
+  residents.forEach((element) => {
+    arrayOfAges.push(element.age);
+  });
+  return arrayOfAges.every((number) => number >= age);
+}
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const selectedAnimalObject = selecionaAnimal(animal);
+  return confereIdade(age, selectedAnimalObject);
 }
 
 module.exports = getAnimalsOlderThan;
