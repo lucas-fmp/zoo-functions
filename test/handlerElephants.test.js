c8ae6383e4b28c63a24c55ffd9a7db93e22ccc52
Lucas <lucasfernandomacedo13@gmail.com>
2022-05-23 16:03:23 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna 4 quando passado "count" como parâmetro da função', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Retorna um número proximo a 10.5 quando o argumento passador for averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
