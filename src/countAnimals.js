884d543a4ca8830dd8b393e8534c8f9fe25f950e
Lucas <lucasfernandomacedo13@gmail.com>
2022-05-23 15:49:07 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,54 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+const emptyParameter = (animal) => {
+  const returnedObject = {};
+  const names = species.map((element) => element.name);
+  const quantity = species.map((element2) => element2.residents.length);
+  for (let index = 0; index < species.length; index += 1) {
+    returnedObject[names[index]] = quantity[index];
+  }
+  return returnedObject;
+};
+
+const justSpecies = (animal) => {
+  let counter = 0;
+  const animalName = Object.values(animal[0])[0];
+  species.forEach((element) => {
+    if (element.name === animalName) {
+      counter += element.residents.length;
+    }
+  });
+  return counter;
+};
+
+const speciesAndSex = (animal) => {
+  let counter = 0;
+  const animalName = Object.values(animal[0])[0];
+  const sex = Object.values(animal[0])[1];
+  species.forEach((element) => {
+    if (element.name === animalName) {
+      element.residents.forEach((element2) => {
+        if (element2.sex === sex) {
+          counter += 1;
+        }
+      });
+    }
+  });
+  return counter;
+};
+
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    return emptyParameter(animal);
+  }
+  if (Object.keys(animal[0]).length === 1) {
+    return justSpecies(animal);
+  }
+  if (Object.keys(animal[0]).length === 2) {
+    return speciesAndSex(animal);
+  }
 }
 
 module.exports = countAnimals;
