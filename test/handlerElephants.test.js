e3dff2e3911fe27a645468b5cafb54b04d1a2f77
Lucas <lucasfernandomacedo13@gmail.com>
2022-05-23 16:13:08 -0300

    Requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Retorna um número proximo a 10.5 quando o argumento passador for averageAge', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('Retorna "NW" quando o argumento passador for location', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Retorna um número igual ou maior a 5 quando o argumento passador for popularity', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Retorna um array de dias que não contém "Monday" quando o argumento passador for availability', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+  it('Retorna undefined se não for passado nenhum argumento', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
 });
