8b1634e2972b9a6eb35787ac5a15c79ef020005c
Lucas <lucasfernandomacedo13@gmail.com>
2022-05-23 17:57:09 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,45 @@
 const data = require('../data/zoo_data');
 
+const visitantes = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aquii
+  const quantityOfEntrants = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      quantityOfEntrants.child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      quantityOfEntrants.adult += 1;
+    }
+    if (element.age >= 50) {
+      quantityOfEntrants.senior += 1;
+    }
+  });
+  return quantityOfEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  let total = 0;
+  const object = countEntrants(entrants);
+  const values = Object.values(object);
+  total = (values[0] * 20.99) + (values[1] * 49.99) + (values[2] * 24.99);
+  return total;
 }
+
+calculateEntry({});
 
 module.exports = { calculateEntry, countEntrants };
