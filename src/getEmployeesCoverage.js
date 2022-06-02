6cca9b0d653cf336f117a20a878c2940eef41199
Lucas <lucasfernandomacedo13@gmail.com>
2022-06-02 09:37:29 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,100 +1,42 @@
 const data = require('../data/zoo_data');
 
-const { employees, species } = data;
-const employeeInfos = {};
-
-const arrayOfSpecies = (arrayOfIds) => {
-  const animals = [];
-  species.forEach((animal) => {
-    arrayOfIds.forEach((Id) => {
-      if (animal.id.includes(Id)) {
-        animals.push(animal.name);
-      }
-    });
-  });
-  return animals;
-};
-
-const arrayOfLocations = (animalNames) => {
-  const speciesArr = [];
-  const locationsArr = [];
-  species.forEach((specie) => {
-    animalNames.forEach((animalName) => {
-      if (specie.name === animalName) {
-        speciesArr.push(specie);
-      }
-    });
-  });
-  speciesArr.forEach((specie) => {
-    locationsArr.push(specie.location);
-  });
-  return locationsArr;
-};
-
-const getEmployeeByName = (obj) => {
-  employees.forEach((employee) => {
-    if (employee.firstName === obj.name || employee.lastName === obj.name) {
-      employeeInfos.id = employee.id;
-      employeeInfos.fullName = `${employee.firstName} ${employee.lastName}`;
-      employeeInfos.species = arrayOfSpecies(employee.responsibleFor);
-      employeeInfos.locations = arrayOfLocations(arrayOfSpecies(employee.responsibleFor));
+function getAnimalsNames(employeeResponsibleFor) {
+  const animalsName = [];
+  employeeResponsibleFor.forEach((employeeAnimalId) => data.species.forEach((specie) => {
+    if (employeeAnimalId === specie.id) {
+      animalsName.push(specie.name);
     }
-  });
-  try {
-    if (Object.keys(employeeInfos).length !== 0) return employeeInfos;
-    throw new Error('Informações inválidas');
-  } catch (error) {
-    throw error.message;
-  }
-};
-
-const getEmployeeById = ((obj) => {
-  employees.forEach((employee) => {
-    if (employee.id === obj.id) {
-      employeeInfos.id = employee.id;
-      employeeInfos.fullName = `${employee.firstName} ${employee.lastName}`;
-      employeeInfos.species = arrayOfSpecies(employee.responsibleFor);
-      employeeInfos.locations = arrayOfLocations(arrayOfSpecies(employee.responsibleFor));
-    }
-  });
-  try {
-    if (Object.keys(employeeInfos).length !== 0) return employeeInfos;
-    throw new Error('Informações inválidas');
-  } catch (error) {
-    throw error.message;
-  }
-});
-
-const getAllEmployees = () => {
-  const arrayyy = [];
-  employees.forEach((employee) => {
-    const objetooo = {
-      id: employee.id,
-      fullName: `${employee.firstName} ${employee.lastName}`,
-      species: arrayOfSpecies(employee.responsibleFor),
-      locations: arrayOfLocations(arrayOfSpecies(employee.responsibleFor)),
-    };
-    arrayyy.push(objetooo);
-  });
-  return arrayyy;
-};
-
-const getEmployeeByIdOrName = (obj) => {
-  if (Object.keys(obj).includes('name')) {
-    return getEmployeeByName(obj);
-  }
-  if (Object.keys(obj).includes('id')) {
-    return getEmployeeById(obj);
-  }
-};
-
-function getEmployeesCoverage(obj) {
-  if (obj !== undefined) {
-    return getEmployeeByIdOrName(obj);
-  }
-  return getAllEmployees();
+  }));
+  return animalsName;
 }
 
-getEmployeesCoverage({ id: 'Id inválido' });
+function getAnimalsLocations(employeeResponsibleFor) {
+  const animalsLocation = [];
+  employeeResponsibleFor.forEach((employeeAnimalId) => data.species.forEach((specie) => {
+    if (employeeAnimalId === specie.id) {
+      animalsLocation.push(specie.location);
+    }
+  }));
+  return animalsLocation;
+}
+
+function getEmployeesCoverage(obj) {
+  const allEmployees = data.employees.map((employee) => {
+    const employeeInfo = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getAnimalsNames(employee.responsibleFor),
+      locations: getAnimalsLocations(employee.responsibleFor),
+    };
+    return employeeInfo;
+  });
+  if (obj === undefined) return allEmployees;
+  if (obj.id || obj.name) {
+    const selectedEmployee = allEmployees
+      .filter((employee) => employee.fullName.includes(obj.name) || employee.id.includes(obj.id));
+    if (selectedEmployee[0] !== undefined) return selectedEmployee[0];
+    throw new Error('Informações inválidas');
+  }
+}
 
 module.exports = getEmployeesCoverage;
